56585f1d88db356f6eb403a4bf04d04042b4a18b
Per Edward Nilsen <nilliz85@gmail.com>
2023-05-12 13:39:25 +0200

    registration, login functionality

--- js/auth.js
+++ js/auth.js
@@ -15,6 +15,26 @@ const API_BASE_URL = "https://nf-api.onrender.com";
  * ```
  */
 
+/* Without modal */
+// async function registerUser(url, userData) {
+//   console.log(url, userData);
+//   try {
+//     const postData = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(userData),
+//     };
+//     const response = await fetch(url, postData);
+//     const json = await response.json();
+//     console.log(json);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+/* with modal */
 async function registerUser(url, userData) {
   console.log(url, userData);
   try {
@@ -27,46 +47,104 @@ async function registerUser(url, userData) {
     };
     const response = await fetch(url, postData);
     const json = await response.json();
+    console.log(json);
+    // If the registration was successful, show the success modal
+    if (response.status === 201) {
+      const modal = new bootstrap.Modal(document.getElementById("successModal"));
+      modal.show();
+      // Redirect the user to the login page after 3 seconds
+      setTimeout(() => {
+        window.location.href = "/login.html";
+      }, 3000);
+    } else {
+      // If the registration was not successful, show the error modal
+      const modal = new bootstrap.Modal(document.getElementById("errorModal"));
+      modal.show();
+    }
   } catch (error) {
     console.error(error);
   }
 }
 
-const userToRegister = {
-  name: "pernilsen", // Required
-  email: "pernil58363@stud.noroff.no", // Required
-  password: "bellanutella", // Required
-};
+const registrationForm = document.querySelector("#registration-form");
+registrationForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent the default form submit behaviour
 
-const registerUrl = `${API_BASE_URL}/api/v1/social/auth/register`;
+  // Get the values from the form
+  const name = document.querySelector("#account-name").value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  const passwordConfirm = document.querySelector("#password-confirm").value;
+
+  // Check if the passwords match
+  if (password !== passwordConfirm) {
+    alert("Passwords do not match");
+    return;
+  }
+
+  // Create the user object
+  const userToRegister = {
+    name: name,
+    email: email,
+    password: password,
+  };
+
+  const registerUrl = `${API_BASE_URL}/api/v1/social/auth/register`;
+  registerUser(registerUrl, userToRegister);
+});
+
+//hardcoded user for testing
+// const userToRegister = {
+//   name: "pernilsen", // Required
+//   email: "pernil58363@stud.noroff.no", // Required
+//   password: "bellanutella", // Required
+// };
+
+// const registerUrl = `${API_BASE_URL}/api/v1/social/auth/register`;
 
 /*---------- Login ----------*/
+//Hardcoded user for testing
+// async function loginUser(url, userData) {
+//   try {
+//     const postData = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(userData),
+//     };
+//     const response = await fetch(url, postData);
+//     console.log(response);
+//     const json = await response.json();
+//     console.log(json);
+//     const accessToken = json.accessToken;
+//     localStorage.setItem("accessToken", accessToken);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// const userToLogin = {
+//   email: "pernil58363@stud.noroff.no",
+//   password: "bellanutella",};
+// const loginUrl = `${API_BASE_URL}/api/v1/social/auth/login`;
+// loginUser(loginUrl, userToLogin);
 
-async function loginUser(url, userData) {
-  try {
-    const postData = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    };
-    const response = await fetch(url, postData);
-    console.log(response);
-    const json = await response.json();
-    console.log(json);
-    const accessToken = json.accessToken;
-    localStorage.setItem("accessToken", accessToken);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// select the login form element and add an event listener to it
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevent the default form submission behavior
 
-const userToLogin = {
-  email: "pernil58363@stud.noroff.no",
-  password: "bellanutella",
-};
+  // get the form input values
+  const email = document.querySelector("#email-input").value;
+  const password = document.querySelector("#password-input").value;
 
-const loginUrl = `${API_BASE_URL}/api/v1/social/auth/login`;
+  // create the user object to be passed as the second argument to loginUser
+  const userToLogin = {
+    email: email,
+    password: password,
+  };
 
-loginUser(loginUrl, userToLogin);
+  // make the API call to log in the user
+  const loginUrl = `${API_BASE_URL}/api/v1/social/auth/login`;
+  loginUser(loginUrl, userToLogin);
+});
