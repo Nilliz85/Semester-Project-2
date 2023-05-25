1384d1c4a9dcf68d00eb6c5886b9fa58058f6579
Per Edward Nilsen <nilliz85@gmail.com>
2023-05-25 17:01:57 +0200

    router file/functionality added

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,33 +1,3 @@
-import * as listeners from "./handlers/index.mjs";
-import * as templates from "./templates/index.mjs";
-import * as postMethods from "./api/posts/index.mjs";
+import router from "./router.mjs";
 
-const path = location.pathname;
-
-if (path === "/profile/login/") {
-  listeners.setLoginFormListener();
-} else if (path === "/profile/register/") {
-  listeners.setRegisterFormListener();
-} else if (path === "/post/create/") {
-  listeners.setCreatePostFormListener();
-} else if (path === "/post/edit/") {
-  listeners.setUpdatePostListener();
-} else if (path === "/profile/edit/") {
-  listeners.setUpdateProfileListener();
-}
-
-// async function testTemplate() {
-//   const posts = await postMethods.getPosts();
-//   const container = document.querySelector("#posts");
-//   templates.renderPostTemplates(posts, container);
-// }
-
-// testTemplate();
-
-// async function testTemplate() {
-//   const posts = await postMethods.getPosts();
-//   const container = document.querySelector("#posts");
-//   templates.renderPostTemplate(post, container);
-// }
-
-// testTemplate();
+router();
