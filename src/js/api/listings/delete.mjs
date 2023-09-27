1dc39998fbfc6b0a7f7b918545a8995d12e8b812
Per Edward Nilsen <nilliz85@gmail.com>
2023-09-27 16:28:35 +0200

    Added descrition to the mjs files in listing

--- src/js/api/listings/delete.mjs
+++ src/js/api/listings/delete.mjs
@@ -1,19 +1,22 @@
-import { API_AUCTION_URL } from '../constants.mjs';
+import { API_AUCTION_URL } from '../../api/constants.mjs';
 import { authFetch } from '../authFetch.mjs';
 
 const action = '/listings';
 const method = 'delete';
 
+/**
+ * This function deletes a listing.
+ * @param {} id  - The listing id
+ * @returns  {Promise<Object>} A Promise that resolves to the listing deletion response.
+ */
+
 export async function removeListing(id) {
 	if (!id) {
-		throw new Error('Delete requires an ID');
+		throw new Error('Listing ID is required to delete a listing.');
 	}
-
-	const updateListingURL = `${API_AUCTION_URL}${action}/${id}`;
-
-	const response = await authFetch(updateListingURL, {
+	const removeListingURL = `${API_AUCTION_URL}${action}/${id}`;
+	const response = await authFetch(removeListingURL, {
 		method,
 	});
-
 	return await response.json();
 }
--- src/js/api/listings/read.mjs
+++ src/js/api/listings/read.mjs
@@ -1,13 +1,21 @@
-import { API_AUCTION_URL } from '../constants.mjs';
+import { API_AUCTION_URL } from '../../api/constants.mjs';
 import { authFetch } from '../authFetch.mjs';
 
 const action = '/listings';
 
-export async function getListings() {
-	const updateListingURL = `${API_AUCTION_URL}${action}`;
+/**
+ * Retrieves listings, allowing sorting by 'sortType' or filtering by 'tag'.
+ * @param {string} tag - The tag to filter listings by.
+ * @param {string} sortType - The type of sorting to apply.
+ * @param {*} id - (Optional) The ID of a specific listing to retrieve.
+ * @throws {Error} If 'id' is provided but not found.
+ * @returns If 'id' is provided, returns the listing with the given ID; otherwise, returns listings
+ * filtered or sorted based on 'tag' and 'sortType'.
+ */
 
-	const response = await authFetch(updateListingURL);
-
+export async function getListings(tag, sortType) {
+	const getListingsURL = `${API_AUCTION_URL}${action}?_author=true${tag ? `&_tag=${tag}` : ''}${sortType ? `&sort=created&sortOrder=${sortType}` : ''}`;
+	const response = await authFetch(getListingsURL);
 	return await response.json();
 }
 
@@ -15,9 +23,7 @@ export async function getListing(id) {
 	if (!id) {
 		throw new Error('Get requires a listingID');
 	}
-	const getListingURL = `${API_AUCTION_URL}${action}/${id}`;
-
+	const getListingURL = `${API_AUCTION_URL}${action}/${id}?_bids=true&_seller=true`;
 	const response = await authFetch(getListingURL);
-
 	return await response.json();
 }
